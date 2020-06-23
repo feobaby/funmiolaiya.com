4e8521f70132feae23c2ea9646f3fe83fb1b77c3
Funmilayo Olaiya <olufunmilayo335@gmail.com>
2020-06-23 19:11:10 +0100

    add files

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import 'antd/dist/antd.css';
 import { Menu } from 'antd';
-import { Link } from 'react-router-dom';
 import './header.css';
 import github from '../../assets/logos/github.png';
 import linkedin from '../../assets/logos/linkedin.png';
@@ -12,15 +11,16 @@ export default function Header() {
   return (
     <Menu mode="horizontal" className="menu">
       <Menu.Item key="blog">
-        <a href="https://techshrimps.hashnode.dev/" target="_blank"><h2 class="blog">check out my blog posts</h2></a>
+        <a href="https://techshrimps.hashnode.dev/" target="_blank" rel="noopener noreferrer"><h2 class="blog">check out my blog posts</h2></a>
       </Menu.Item>
 
       <Menu.Item key="contact">
-        <a href="https://docs.google.com/forms/d/e/1FAIpQLSfD1pugVOK31ZfERBFIJUnM5gp5XlBunDYb4E-xWBvOX5hVMQ/viewform?usp=sf_link" target="_blank"><h2 class="blog">contact</h2></a>
+        <a href="https://docs.google.com/forms/d/e/1FAIpQLSfD1pugVOK31ZfERBFIJUnM5gp5XlBunDYb4E-xWBvOX5hVMQ/viewform?usp=sf_link" target="_blank" rel="noopener noreferrer">
+        <h2 class="blog">contact</h2></a>
       </Menu.Item>
 
       <Menu.Item key="github">
-        <a href="https://github.com/funmi5" target="_blank">
+        <a href="https://github.com/funmi5" target="_blank" rel="noopener noreferrer">
           <img
             src={github}
             className="photo-github"
@@ -30,7 +30,7 @@ export default function Header() {
       </Menu.Item>
 
       <Menu.Item key="linkedin">
-        <a href="https://www.linkedin.com/in/funmilayo-e-olaiya-167490ba/" target="_blank">
+        <a href="https://www.linkedin.com/in/funmilayo-e-olaiya-167490ba/" target="_blank" rel="noopener noreferrer">
           <img
             src={linkedin}
             className="photo-github"
@@ -40,7 +40,7 @@ export default function Header() {
       </Menu.Item>
 
       <Menu.Item key="codesandbox">
-        <a href="https://codesandbox.io/u/funmi5" target="_blank">
+        <a href="https://codesandbox.io/u/funmi5" target="_blank" rel="noopener noreferrer">
           <img
             src={codesandbox}
             className="photo-github"
@@ -50,7 +50,7 @@ export default function Header() {
       </Menu.Item>
 
       <Menu.Item key="medium">
-        <a href="https://medium.com/@funmiolaiya9" target="_blank">
+        <a href="https://medium.com/@funmiolaiya9" target="_blank" rel="noopener noreferrer">
           <img
             src={medium}
             className="photo-medium"
