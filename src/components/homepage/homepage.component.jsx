b1a40b335d51e53488a36a549ed047db716d7e56
Funmilayo Olaiya <olufunmilayo335@gmail.com>
2021-02-11 13:35:49 +0100

    add update

--- src/components/homepage/homepage.component.jsx
+++ src/components/homepage/homepage.component.jsx
@@ -16,9 +16,8 @@ export default function Homepage() {
                     I am really interested in how tech fits into the future of things, and I prefer <br />
                     to be in the long run by creating more efficient platforms to aid humanity.<br />
                     I am really passionate about advocacy and giving back to the society and<br/>
-                    I have so much interest in Machine Learning and OSS.<br />
-                    I work on the documentation at <a href="https://hasura.io">Hasura</a><br />
-                    I am also a Network L/E for <a href="https://www.womenwhocode.com/lagos"> WomenWhoCode Lagos</a><br />
+                    I also have so much interest in Machine Learning and OSS.<br />
+                    I am currently looking for my next role particularly in Software Engineering!<br />
                     I run a technical blog, you can find my posts at <a href="https://ferns.dev"> Ferns</a>
                     <br />
                 </p>
@@ -35,7 +34,7 @@ export default function Homepage() {
     <p align="left">Hasura &nbsp; JavaScript</p>
       <p>Get Started with Hasura</p>
          <hr />
-      <p>This blog post is about how to create a todo app using Hasura, GraphQl and React.</p>
+      <p>How to create a todo app using Hasura, GraphQl and React.</p>
       <p><a href="https://techshrimps.hashnode.dev/let-us-create-a-todo-app-with-react-hasura-and-graphql" target="_blank" rel="noopener noreferrer">
       <img
                     src={link}
@@ -48,11 +47,11 @@ export default function Homepage() {
   
   <div className="column">
     <div className="card zoom">
-     <p align="left">React &nbsp; JavaScript</p>
-      <p>Create a Search Component with React Hooks.</p>
+     <p align="left">Beginners &nbsp; Back-End</p>
+      <p>For beginners - Roadmap to being a great backend developer.</p>
          <hr />
-      <p>About how to search for generated contents using React.</p>
-      <p><a href="https://techshrimps.hashnode.dev/creating-a-simple-search-field-with-react-hooks-as-a-function-component-ckapt39xw03p84us1marzyqh8" target="_blank" rel="noopener noreferrer">
+      <p>About how beginners can get started in back-end development.</p>
+      <p><a href="https://ferns.dev/for-beginners-roadmap-to-being-a-great-backend-developer" target="_blank" rel="noopener noreferrer">
       <img
                     src={link}
                     className="photo-link"
@@ -79,14 +78,11 @@ export default function Homepage() {
 <br /> 
 <br />
 
-            <footer> 2020 <sup>&copy;</sup> Funmilayo Olaiya <br /><br /><br />
-             we can connect - <a className="links" href="https://github.com/codeliezel" target="_blank" rel="noopener noreferrer"><u>github</u></a>
+            <footer> 2021 <sup>&copy;</sup> Funmilayo Olaiya <br /><br /><br />
+            <a className="links" href="https://github.com/codeliezel" target="_blank" rel="noopener noreferrer"><u>github</u></a>
              &nbsp;
              <a className="links" href="https://www.linkedin.com/in/funmilayo-e-olaiya-167490ba" target="_blank" rel="noopener noreferrer"><u>linkedin</u></a>&nbsp;
-              <a className="links" href="https://codesandbox.io/u/funmi5" target="_blank" rel="noopener noreferrer"><u>codesandbox</u></a>&nbsp;
-               <a className="links" href="https://medium.com/@funmiolaiya9" target="_blank" rel="noopener noreferrer"><u>medium</u></a>&nbsp;
-                <a className="links" href="https://docs.google.com/forms/d/e/1FAIpQLSfD1pugVOK31ZfERBFIJUnM5gp5XlBunDYb4E-xWBvOX5hVMQ/viewform?usp=sf_link" target="_blank" rel="noopener noreferrer"><u>contact</u></a>
-            </footer>
+               </footer>
             <br />
         </>
     );
