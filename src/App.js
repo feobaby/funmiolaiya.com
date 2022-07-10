8a0b7bc8a032f9c7445ada85950df79f94e43a4f
Funmilayo Olaiya <olufunmilayo335@gmail.com>
2022-07-10 18:07:47 +0100

    add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Homepage from './components/homepage/homepage.component';
+import LoadingSpinner from './components/spinner/spinner.component';
 
 export default function App() {
+  const [loading] = useState(false);
+  if (loading) {
+    return (
+      <div>
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
   return (
     <Router>
       <Switch>
