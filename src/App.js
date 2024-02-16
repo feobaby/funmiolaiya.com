85ca6674a47d1c281e27640df2a40b8078037217
feobaby <olufunmilayo335@gmail.com>
2024-02-16 05:51:56 -0500

    add changes to app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./components/Homepage/homepage.component.jsx";
+import HomePage from "./components/Homepage/homepage.component";
 
 export default function App() {
   return (
