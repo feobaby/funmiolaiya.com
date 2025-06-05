51a3133cc835796ddf0764d98ebd08041cbd3a1e
feobaby <olufunmilayo335@gmail.com>
2025-06-05 03:47:06 -0400

    fix some texts

--- src/components/information/hello.jsx
+++ src/components/information/hello.jsx
@@ -4,41 +4,47 @@ import { Box, Text } from '@chakra-ui/react';
 const Hello = () => {
   return (
     <div className="text-focus-in">
-      <Box w="80%" p={12} m={4}>
-        {/* <Heading as="h6" size="xs" color="red.100">
-          Interests
-        </Heading> */}
-        <Text className="mulish" fontSize="8xl" fontWeight="700">
+      <Box w={['95%', '90%', '80%']} p={6} m={4} maxW="1000px" mx="auto">
+        <Text
+          className="mulish"
+          fontSize={['20px', '32px', '48px', '64px']}
+          fontWeight="700"
+        >
           Hello! <br />I am Funmi Olaiya. 👋🏾
         </Text>
         <br />
-        <Text className="outlined-text" fontSize="6xl" fontWeight="700">
+        <Text
+          className="outlined-text"
+          fontSize={['16px', '24px', '36px', '48px']}
+          fontWeight="700"
+        >
           - Software Engineer, Technical Mentor & Generalist
         </Text>
         <br />
         <Text
           className="mulish other-text outlined-text"
-          fontSize="3xl"
+          fontSize={['14px', '18px', '24px', '32px']}
           fontWeight="400"
         >
           My fav. quote is one by <i>Nelson Mandela</i>:
-          <p fontWeight="100">
+          <p style={{ fontWeight: 100, marginTop: '8px' }}>
             There is no passion to be found playing small - in settling for a
             life that is less than the one you are capable of living. 🧠
           </p>
         </Text>
         <br /> <br />
-        <Text className="mulish" fontSize="md" fontWeight="400">
+        <Text className="mulish" fontSize={['12px', '14px']} fontWeight="400">
           Learn more about me here 👉🏾 &nbsp;
           <a href="https://github.com/feobaby" target="_blank" rel="noreferrer">
-            <u>Github</u>
+            <u>Github </u>
           </a>
-          &nbsp; &nbsp;
+          &nbsp;
           <a
             href="https://portfolio.funmiolaiya.com/"
             target="_blank"
             rel="noreferrer"
           >
+            &     &nbsp;
             <u>Portfolio</u>
           </a>
         </Text>
