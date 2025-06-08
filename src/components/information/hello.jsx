81bc88bf2eeb62edfa0d279ebf1f4df41688b8a0
feobaby <olufunmilayo335@gmail.com>
2025-06-07 22:51:51 -0400

    make some more chnages

--- src/components/information/hello.jsx
+++ src/components/information/hello.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
-import { Box, Text } from '@chakra-ui/react';
+import { Box, Text, Flex } from '@chakra-ui/react';
 
 const Hello = () => {
   return (
     <div className="text-focus-in">
-      <Box w={['95%', '90%', '80%']} p={6} m={4} maxW="1000px" mx="auto">
+      <Box
+        w={['95%', '90%', '80%']}
+        p={6}
+        m={4}
+        maxW="1000px"
+        mx="auto"
+        justify="center"
+        align="center"
+      >
         <Text
-          className="mulish"
+          className="outlined-text"
           fontSize={['32px', '48px', '64px']}
           fontWeight="700"
         >
@@ -18,36 +25,76 @@ const Hello = () => {
           fontSize={['24px', '36px', '48px']}
           fontWeight="700"
         >
-          - Software Engineer, Technical Mentor & Generalist
+          Software Engineer, Technical Mentor & Generalist
         </Text>
         <br />
-        <Text
-          className="mulish other-text outlined-text"
-          fontSize={['24px', '32px']}
-          fontWeight="400"
+        <Flex
+          direction="row"
+          justify="center"
+          align="center"
+          wrap="wrap"
+          gap={4}
+          p={4}
         >
-          My fav. quote is one by <i>Nelson Mandela</i>:
-          <p style={{ fontWeight: 100, marginTop: '8px' }}>
-            There is no passion to be found playing small - in settling for a
-            life that is less than the one you are capable of living. 🧠
-          </p>
-        </Text>
-        <br /> <br />
-        <Text className="mulish" fontSize={['14px']} fontWeight="400">
-          Learn more about me here 👉🏾 &nbsp;
-          <a href="https://github.com/feobaby" target="_blank" rel="noreferrer">
-            <u>Github </u>
-          </a>
-          &nbsp;
-          <a
-            href="https://portfolio.funmiolaiya.com/"
-            target="_blank"
-            rel="noreferrer"
+          <Text
+            className="outlined-text"
+            fontSize={['24px', '32px']}
+            fontWeight="200"
           >
-            &     &nbsp;
-            <u>Portfolio</u>
-          </a>
-        </Text>
+            <a
+              href="https://portfolio.funmiolaiya.com/"
+              target="_blank"
+              without
+              rel="noreferrer"
+            >
+              Portfolio
+            </a>
+          </Text>
+          <Text
+            className="outlined-text"
+            fontSize={['24px', '32px']}
+            fontWeight="200"
+          >
+            <a
+              href="https://github.com/feobaby"
+              target="_blank"
+              without
+              rel="noreferrer"
+            >
+              GitHub
+            </a>
+          </Text>
+          <Text
+            className="outlined-text"
+            fontSize={['24px', '32px']}
+            fontWeight="200"
+          >
+            <a
+              href="https://www.linkedin.com/in/funmilayo-e-olaiya-167490ba/"
+              target="_blank"
+              without
+              rel="noreferrer"
+            >
+              {' '}
+              LinkedIn
+            </a>
+          </Text>
+          <Text
+            className="outlined-text"
+            fontSize={['24px', '32px']}
+            fontWeight="200"
+          >
+            <a
+              href="https://blog.funmiolaiya.com/"
+              target="_blank"
+              without
+              rel="noreferrer"
+            >
+              {' '}
+              Blog
+            </a>
+          </Text>
+        </Flex>
       </Box>
     </div>
   );
