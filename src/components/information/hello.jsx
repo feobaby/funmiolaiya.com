6c15bb76b976d61946be63001753a9e98ee58628
feobaby <olufunmilayo335@gmail.com>
2025-08-29 02:58:33 -0500

    create a new overall look

--- src/components/information/hello.jsx
+++ src/components/information/hello.jsx
@@ -1,102 +1,105 @@
-import { Box, Text, Flex } from '@chakra-ui/react';
+import { Box, Text, Flex, VStack, Button, Link } from "@chakra-ui/react";
+import imageData from "../data.js";
 
 const Hello = () => {
   return (
-    <div className="text-focus-in">
-      <Box
-        w={['95%', '90%', '80%']}
-        p={6}
-        m={4}
-        maxW="1000px"
-        mx="auto"
-        justify="center"
-        align="center"
-      >
-        <Text
-          className="outlined-text"
-          fontSize={['32px', '48px', '64px']}
-          fontWeight="700"
-        >
-          Hello! <br />I am Funmi Olaiya. 👋🏾
+    <VStack spacing={8} py={10}>
+      {/* Intro Section */}
+      <Box textAlign="center" maxW="600px" px={4}>
+        <Text fontSize="3xl" fontWeight="bold" color="#ffffff">
+          Hey, I’m Funmi 👋
         </Text>
-        <br />
-        <Text
-          className="outlined-text"
-          fontSize={['24px', '36px', '48px']}
-          fontWeight="700"
-        >
-          Software Engineer, Technical Mentor, Writer & Generalist
+        <Text fontSize="md" mt={2} color="#aaaaaa">
+          I’m a full-stack developer, writer, and DJ. Here’s a little tour of my work, blogs, and passions.
         </Text>
-        <br />
-        <Flex
-          direction="row"
-          justify="center"
-          align="center"
-          wrap="wrap"
-          gap={4}
-          p={4}
-        >
-          <Text
-            className="outlined-text"
-            fontSize={['24px', '32px']}
-            fontWeight="200"
-          >
-            <a
-              href="https://portfolio.funmiolaiya.com/"
-              target="_blank"
-              without
-              rel="noreferrer"
-            >
-              Portfolio
-            </a>
-          </Text>
-          <Text
-            className="outlined-text"
-            fontSize={['24px', '32px']}
-            fontWeight="200"
-          >
-            <a
-              href="https://github.com/feobaby"
-              target="_blank"
-              without
-              rel="noreferrer"
-            >
-              GitHub
-            </a>
-          </Text>
-          <Text
-            className="outlined-text"
-            fontSize={['24px', '32px']}
-            fontWeight="200"
-          >
-            <a
-              href="https://www.linkedin.com/in/funmilayo-e-olaiya-167490ba/"
-              target="_blank"
-              without
-              rel="noreferrer"
-            >
-              {' '}
-              LinkedIn
-            </a>
-          </Text>
-          <Text
-            className="outlined-text"
-            fontSize={['24px', '32px']}
-            fontWeight="200"
-          >
-            <a
-              href="https://blog.funmiolaiya.com/"
-              target="_blank"
-              without
-              rel="noreferrer"
-            >
-              {' '}
-              Blog
-            </a>
-          </Text>
-        </Flex>
       </Box>
-    </div>
+
+      {/* Cards Section */}
+      <Flex direction="row" wrap="wrap" gap={6} justify="center" px={4}>
+        {imageData.map((item, index) => {
+          const Icon = item.icon;
+          return (
+            <Box
+              key={index}
+              flexBasis={{ base: "100%", md: "30%" }}
+              bg={item.color}
+              borderRadius="15px"
+              p={6}
+              boxShadow="lg"
+              minHeight="260px"
+              display="flex"
+              flexDirection="column"
+              justifyContent="space-between"
+              alignItems="center"
+              textAlign="center"
+              transition="all 0.3s ease"
+              _hover={{
+                transform: `rotate(${item.tilt}) scale(1.05)`,
+                boxShadow: "xl",
+              }}
+            >
+              {/* Content Wrapper */}
+              <Flex direction="column" align="center" justify="center" flex="1">
+                {/* Icon */}
+                <Icon
+                  size="40px"
+                  color={item.textColor}
+                  style={{ marginBottom: "12px" }}
+                />
+
+                {/* Main Name */}
+                <Text
+                  className="fredoka"
+                  fontSize="5xl"
+                  fontWeight="medium"
+                  color={item.nameColor}
+                >
+                  {item.name}
+                </Text>
+
+                {/* Title */}
+                <Text fontSize="md" mt={2} color={item.textColor}>
+                  {item.title}
+                </Text>
+
+                {/* Tags */}
+                <Flex wrap="wrap" justify="center" mt={3} gap={2}>
+                  {item.tags?.map((tag, i) => (
+                    <Box
+                      key={i}
+                      px={2}
+                      py={1}
+                      bg="#333"
+                      borderRadius="5px"
+                      fontSize="xs"
+                      color="#cccccc"
+                    >
+                      {tag}
+                    </Box>
+                  ))}
+                </Flex>
+              </Flex>
+
+              {/* Button */}
+              {item.link && (
+                <Link href={item.link} isExternal={item.link.startsWith("http")}>
+                  <Button
+                    mt={4}
+                    size="sm"
+                    bg="#333"
+                    color="#cccccc"
+                    borderRadius="md"
+                    _hover={{ bg: "#555", transform: "scale(1.05)" }}
+                  >
+                    {item.buttonText}
+                  </Button>
+                </Link>
+              )}
+            </Box>
+          );
+        })}
+      </Flex>
+    </VStack>
   );
 };
 
